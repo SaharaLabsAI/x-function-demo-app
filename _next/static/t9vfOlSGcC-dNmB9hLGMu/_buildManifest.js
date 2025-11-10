0610bd9ac8e434a73bd2dd90802c9835d5f7e10e
DD <w734275083@163.com>
2025-11-10 17:51:14 +0800

    Updates

---
+++ _next/static/t9vfOlSGcC-dNmB9hLGMu/_buildManifest.js
@@ -0,0 +1,14 @@
+self.__BUILD_MANIFEST = {
+  "/_error": [
+    "./static/chunks/7f0322c69d3d8b74.js"
+  ],
+  "__rewrites": {
+    "afterFiles": [],
+    "beforeFiles": [],
+    "fallback": []
+  },
+  "sortedPages": [
+    "/_app",
+    "/_error"
+  ]
+};self.__BUILD_MANIFEST_CB && self.__BUILD_MANIFEST_CB()
